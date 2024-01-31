77027f711ace26ecf1113367a1de6c6dd8069453
ShreyasTheRag <79953747+ShreyasTheRag@users.noreply.github.com>
2024-01-31 15:00:19 -0800

    changes again

--- js/login.js
+++ js/login.js
@@ -7,7 +7,13 @@ document.getElementById('submitbutton').addEventListener('click', do);*/
 $(function() {
     $("#submitbutton").click(
         function() {
-            alert("Hello " + $('#username').prop("value"));
+            //alert("Hello " + $('#username').prop("value"));
+            var username = $('#username').prop('value');
+            var password = $('#password').prop('value');
+            var message = "Your username is " + username;
+            var message2 = "Your password is " + password;
+            alert(message);
+            alert(message2);
         }
     )
 })
