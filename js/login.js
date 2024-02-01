96e67048866eb06c96021e3098092b21ae98dbb8
ShreyasTheRag <79953747+ShreyasTheRag@users.noreply.github.com>
2024-01-31 16:03:13 -0800

    wow what work

--- js/login.js
+++ js/login.js
@@ -5,15 +5,24 @@ function do() {
 // external.js or in another <script> tag in HTML
 document.getElementById('submitbutton').addEventListener('click', do);*/
 $(function() {
+    var users = new Map();
     $("#submitbutton").click(
         function() {
             //alert("Hello " + $('#username').prop("value"));
             var username = $('#username').prop('value');
             var password = $('#password').prop('value');
-            var message = "Your username is " + username;
-            var message2 = "Your password is " + password;
-            alert(message);
-            alert(message2);
+            //var message = "Your username is " + username;
+            //var message2 = "Your password is " + password;
+            if (users.has(username)) {
+                if (users.get(username) !== password) {
+                    alert("Incorrect password. Try again.");
+                } else {
+                    alert("You logged in as " + username + " and your password is " + "I ain't telling you shit".substring(0, password.length));
+                }
+            } else {
+                alert("Your account has been added");
+                users.set(username, password);
+            }
         }
     )
 })
