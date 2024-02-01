ba3d1437d09c055ae90e981fcde53f5aff0edf45
Francisco Salinas <salinasf3@gmail.com>
2024-02-01 11:40:28 -0800

    added file storage parsing

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -8,22 +8,15 @@ let selector = document.getElementById("id");
 form.addEventListener('submit',(event)=>{
     event.preventDefault();
     const formData = new FormData(form); // Create a FormData object from the form
-    fetch(`http://localhost:8080/projects?id=${formData.get("id")}`, { // Replace 'your-server-endpoint' with your server URL
-        method: 'GET',
-    })
-    .then(response => response.json()) // Assuming the server responds with JSON
-    .then(data => {
-        doc.textContent = JSON.stringify(data);
-    })
-    .catch(error => console.error('Error:', error));
+    fetchproject(formData.get("id"));
 })
 
 
-function fetchproject(){
-    fetch("http://localhost:8080/projects/1").then(response=>{
+function fetchproject(id){
+    fetch(`http://localhost:8080/projects/?id=${id}`).then(response=>{
         return response.ok ? response.json() : Promise.reject(response.status);
     }).then(responsejson=>{
-        doc.textContent = JSON.stringify(responsejson);
+        doc.textContent = JSON.stringify(responsejson,null,2);
     });
 }
 
