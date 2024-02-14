a69c5db952ad835e9ae07a8eda1231b900d40a0b
Francisco Salinas <salinasf3@gmail.com>
2024-02-14 15:06:38 -0800

    changed server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const port = 8080;
-const projects = require('.project-storage.js');
 const cors = require("cors");
 const path = require('path');
 const fs = require("fs");
