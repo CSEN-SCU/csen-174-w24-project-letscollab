19ae9dafcda1518ce9229815d03b1f2ae853c90a
ShreyasTheRag <79953747+ShreyasTheRag@users.noreply.github.com>
2024-02-14 15:48:28 -0800

    breh

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const app = express();
-const port = 8080;
-//const projects = require('.project-storage.js');\
 const cors = require("cors");
 const path = require('path');
 const fs = require("fs");
