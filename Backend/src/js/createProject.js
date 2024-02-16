97d2d16af25ba3ac632ff091298e140b01ff0ec7
Eval <zcommon03@gmail.com>
2024-02-16 09:29:22 -0800

    Updates for realtime updating of createProject.html

--- Backend/src/js/createProject.js
+++ Backend/src/js/createProject.js
@@ -1,5 +1,5 @@
 // On page load create event listeners
-$(() => {
+$(async () => {
     // Create input listener for uploading project image
     $("#photo").on("input", (event) => {
         updatePreviewImage(event);
@@ -31,7 +31,7 @@ $(() => {
     });
 
     // Load skill list
-    loadSkillList();
+    await loadSkillList();
 });
 
 /**
@@ -45,6 +45,14 @@ const updatePreviewImage = (event) => {
     const image = URL.createObjectURL(images[0]);
     const previewElement = $("#projectlist img");
     previewElement.attr("src", image);
+
+    // Create status message
+    const status = $("#uploadstatus");
+    status.html("Upload Successful!");
+    status.css({
+        "color": "green",
+        "font-size": "small",
+    });
 }
 
 /**
@@ -74,10 +82,10 @@ const updatePreviewDescription = () => {
 }
 
 /**
- * Updates the meetup time of the preview in realtime (the regex is ugly, and I'm sorry)
+ * Updates the meetup time of the preview in realtime (the regex is ugly, and I'm sorry) <-- its not that bad anymore
  */
 const updatePreviewDateTime = () => {
-    const previewElement = $("#projectlist #meetup");
+    const previewElement = $("#projectlist #meettime");
 
     // Number to text month object
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -95,7 +103,7 @@ const updatePreviewDateTime = () => {
         const timeResult = `${hour}:${minutes} ${timeSuffix}`;
 
         // Update preview element
-        previewElement.html(previewElement.html().replace(/(Meetup Time:\s)(.+)(\sLocation:\s.+)/g, `$1${timeResult}$3`));
+        previewElement.html(previewElement.html().replace(/(Meetup Time:\s)(.+)/g, `$1${timeResult}`));
     }
 
     // Gather date data
@@ -109,7 +117,7 @@ const updatePreviewDateTime = () => {
         const dateResult = `${months[month]} ${day}, ${year}`;
 
         // Update preview element
-        previewElement.html(previewElement.html().replace(/(Meetup Time:\s)(.+)(\sLocation:\s.+)/g, `$1${dateResult} at $2$3`));
+        previewElement.html(previewElement.html().replace(/(Meetup Time:\s)(.+)/g, `$1${dateResult} at $2`));
     }
 }
 
@@ -118,28 +126,87 @@ const updatePreviewDateTime = () => {
  */
 const updatePreviewLocation = () => {
     const locationContent = $("#location").val();
-    const previewElement = $("#projectlist #meetup");
+    const previewElement = $("#projectlist #meetlocation");
 
     if (locationContent.length > 1) {
-        previewElement.html(previewElement.html().replace(/(.+Location:\s)(.+)/g, `$1${locationContent}`));
+        previewElement.html(previewElement.html().replace(/(Location:\s)(.+)/g, `$1${locationContent}`));
     } else {
-        previewElement.html(previewElement.html().replace(/(.+Location:\s)(.+)/g, "$1x"));
+        previewElement.html(previewElement.html().replace(/(Location:\s)(.+)/g, "$1x"));
     }
 }
 
 /**
  * Loads the general index of skills from skills.json
  */
-const loadSkillList = () => {
-    let skills;
-    $.ajax({
+const loadSkillList = async () => {
+    let skills = {};
+    await $.ajax({
         url: "/v1/getSkills",
         type: "GET",
-        success:function(response, textStatus, xhr) {
-            skills = response.data;
+        success: function(response, textStatus, xhr) {
+            skills = response.data["skillList"];
         },
-        error:function(xhr, status, error) {
-            console.log("god dammit");
+        error: function(xhr, status, error) {
+            console.log("this is bad. very bad");
         }
+    });
+
+    // Add all skills to the proper container
+    const container = $("#selectskills");
+    skills.forEach((skill) => {
+        createSkillElement(container, skill, false);
+    });
+}
+
+/**
+ * Creates a skill element in a given container
+ * @param {object}container Where to put the skill
+ * @param {object}skill What the skill is in format { skillName: "name", skillType: "type"}
+ * @param {boolean}isPreview Whether the skill is being appended to the preview view or not
+ */
+const createSkillElement = (container, skill, isPreview) => {
+    const newSkill = $("<div>");
+    const newSkillIcon = $("<p>");
+    const newSkillName = $("<p>");
+
+    // Add event listener to skills only if it is NOT a preview item
+    if (!isPreview) {
+        newSkill.click(() => {
+            // If the skill exists in the project, remove it
+            if (newSkill.hasClass("selected")) {
+                newSkill.removeClass("selected");
+                removeSkillElement(skill.skillName);
+            } else { // Otherwise, add it
+                newSkill.addClass("selected");
+                const previewContainer = $("#projectlist .skills");
+                createSkillElement(previewContainer, skill, true)
+            }
+        });
+    }
+
+
+    // Add proper classes and content
+    newSkill.addClass(`skill ${skill.skillType}`);
+
+    newSkillIcon.addClass("skillicon");
+    newSkillIcon.text("•");
+
+    newSkillName.addClass("skillname");
+    newSkillName.text(skill.skillName);
+
+    // Create element hierarchy
+    newSkill.append(newSkillIcon);
+    newSkill.append(newSkillName);
+    container.append(newSkill);
+}
+
+/**
+ * Removes a skill element from the preview based on the name of the skill
+ * @param {string}skillName Name of skill
+ */
+const removeSkillElement = (skillName) => {
+    const skills = $("#projectlist .skills").children();
+    skills.each((index, skill) => {
+        if ($(skill).find(".skillname").html() === skillName) skills.eq(index).remove();
     })
 }
