a57bc3173033bec16773c218fc45ff48115262c7
ShreyasTheRag <79953747+ShreyasTheRag@users.noreply.github.com>
2024-02-15 17:09:04 -0800

    fix whitespace and equal signs

--- Backend/src/js/login.js
+++ Backend/src/js/login.js
@@ -5,34 +5,33 @@ const createButton = document.getElementById("create");
 createButton.addEventListener("click", () => {
     window.location.href = "/profile";
 });
-loginForm.addEventListener("submit",(event)=>{
+loginForm.addEventListener("submit",(event) => {
     event.preventDefault();
     const form = new FormData(loginForm);
-    if(form.get("email")==""){
+    if (form.get("email") === "") {
         return $('#response').html("Missing Username").css("color","red");
     }
-    if(form.get("password")==""){
+    if (form.get("password") === "") {
         return $('#response').html("Missing Password").css("color","red");
     }
     $('#response').html('');
     API.getLogin(form.get("email"),form.get("password")).then(data=>{
-        if(data.status){
-                localStorage.clear();
-                for(const [key,value] of Object.entries(data.data)){                
-                    localStorage.setItem(key,value);
-                }
-                $('#response').html(`${data.response}`).css("color","green");
-                setTimeout(()=>{
-                   window.location.href = "/profile"
-                },1500)
-            }else{
-                $('#response').html(`${data.response}`).css("color","red");
-                setTimeout(()=>{
-                    $("#response").html("");
-                },1500)
+        if (data.status) {
+            localStorage.clear();
+            for (const [key,value] of Object.entries(data.data)) {
+                localStorage.setItem(key,value);
             }
-            
-        }).catch(err=>{//error handling
+            $('#response').html(`${data.response}`).css("color","green");
+            setTimeout(() => {
+               window.location.href = "/profile"
+            },1500)
+        } else {
+            $('#response').html(`${data.response}`).css("color","red");
+            setTimeout(() => {
+                $("#response").html("");
+            },1500)
+        }
+        }).catch(err => {//error handling
             $('#response').html('Network Error').css('color','red');
-        })
+        });
 })
