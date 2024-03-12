a35c519269890031a656f08bf1e58eb0b9d497a4
Francisco Salinas <salinasf3@gmail.com>
2024-03-12 10:24:33 -0700

    skill Creation

--- Backend/routes/createSkill.js
+++ Backend/routes/createSkill.js
@@ -1,13 +1,18 @@
 const skills = require('../skill-storage.js');
+const users = require('../user-storage.js')
 module.exports = {
     name: "createSkill",
     method:'POST',
-    execute(body){
+    execute(body,req){
         let out_obj = {};
         return new Promise(resolve=>{
+            let email = req.session.Email;
             let skillName = body.name;
             let skillType = body.type;
+            let userobj = users.getItem(email);
+
             if(skillType==null || skillType == ""){skillType="cs"}
+            if(userobj!=null){
             if(skillName!=null && skillType !=null){
                 let skillObject = skills.getItem(skillName);
                 if(skillObject==null){
@@ -19,7 +24,15 @@ module.exports = {
                     let newSkillObject = skills.getItem(skillName);
                     if(newSkillObject!=null){
                         out_obj["Skill"] = newSkillObject
-                        out_obj["response"] = "Skill added!";
+                    
+                        let userSkills = userobj["Skills"];
+                        if(userSkills.includes(skillName)){
+                            out_obj["response"] = "User already has skill";
+                        }else{
+                            userobj["Skills"].push(skillName);
+                            users.setItem(email,userobj);
+                            out_obj["response"] = "Skill created and added to User!";
+                        }
                     }else{
                         out_obj["response"] = "Error adding skill";
                     }
@@ -29,6 +42,9 @@ module.exports = {
             }else{
                 out_obj["response"] = "Skilltype or skillname is null";
             }
+        }else{
+            out_obj["response"] = "User does not exists. Cannot make request."
+        }
             resolve(out_obj);   
      
         });
