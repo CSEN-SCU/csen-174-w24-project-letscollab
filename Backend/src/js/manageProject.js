d38b2c6939ad7e090a08698ecea4988b25555e4e
Eval <zcommon03@gmail.com>
2024-03-13 16:45:22 -0700

    Cleanup V2

--- Backend/src/js/manageProject.js
+++ Backend/src/js/manageProject.js
@@ -99,7 +99,7 @@ $("#editproject").click(function() {
     window.location.href = `/editProject?id=${id}`;
 });
 
-$("#deleteproject").click(function() {
+$("#deleteproject").click(async () => {
         await API.deleteProject(projectData.ID);
         window.location.href = "/projects";
 });
@@ -130,8 +130,7 @@ const createSkillElement = (container, skill) => {
     const newSkillName = $("<p>");
 
     // Add proper classes and content
-    newSkill.addClass(`
-}skill ${skill.skillType}`);
+    newSkill.addClass(`skill ${skill.skillType}`);
 
     newSkillIcon.addClass("skillicon");
     newSkillIcon.text("•");
